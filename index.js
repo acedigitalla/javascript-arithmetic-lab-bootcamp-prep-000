28dc62f2c680b12d2061bb517a355a34eb1f47bb
Alex Edwards <info@acedigitalagency.net>
2018-03-22 04:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var number = 10
 
-function add (a, b) {
+function add(a, b) {
   return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
   return a / b;
 }
 
-function inc (n) {
+function inc(n) {
   return ++n;
 }
 
-function dec (n) {
+function dec(n) {
   return --n;
 }
